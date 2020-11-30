addb25e89362aedad92631ba2affec9951b0a2be
Alex Florensa <florensa14@gmail.com>
2020-11-30 13:32:55 +0100

    error for test manager

--- demo/demo/darknetTR.cpp
+++ demo/demo/darknetTR.cpp
@@ -56,23 +56,17 @@ tk::dnn::Yolo3Detection* load_network(char* net_cfg, int n_classes, int n_batch)
     std::string net;
     net = net_cfg;
     tk::dnn::Yolo3Detection *detNN = new tk::dnn::Yolo3Detection;
-    detNN->init(net, n_classes, n_batch);
+    detNN->init(net, n_classes, 1);
 
     return detNN;
 }
 #include <typeinfo>
-void do_inference(tk::dnn::Yolo3Detection *net, image im_1, image im_2, image im_3, image im_4)
+void do_inference(tk::dnn::Yolo3Detection *net, image im)
 {
     std::vector<cv::Mat> batch_dnn_input;
-    cv::Mat frame_1(im_1.h, im_1.w, CV_8UC3, (unsigned char*)im_1.data);
-    cv::Mat frame_2(im_2.h, im_2.w, CV_8UC3, (unsigned char*)im_2.data);
-    cv::Mat frame_3(im_3.h, im_3.w, CV_8UC3, (unsigned char*)im_3.data);
-    cv::Mat frame_4(im_4.h, im_4.w, CV_8UC3, (unsigned char*)im_4.data);
-    batch_dnn_input.push_back(frame_1);
-    batch_dnn_input.push_back(frame_2);
-    batch_dnn_input.push_back(frame_3);
-    batch_dnn_input.push_back(frame_4);
-    net->update(batch_dnn_input, 4);
+    cv::Mat frame(im_1.h, im_1.w, CV_8UC3, (unsigned char*)im.data);
+    batch_dnn_input.push_back(frame);
+    net->update(batch_dnn_input, 1);
 
 }
 
